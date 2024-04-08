e225abf25b1e1d7e5d60c01d658cc84943fd88da
Jon Loftin <126590499+jloftin@users.noreply.github.com>
2024-04-08 11:46:11 -0500

    Delete ExerciseFiles/3-TrainingCNNFromScratch directory

--- ExerciseFiles/3-TrainingCNNFromScratch/03-AdditionalFiles/viewMNISTMontage.m
+++
@@ -1,7 +0,0 @@
-function viewMNISTMontage(labelsTrain,imgDataTrain)
-warning off images:imshow:magnificationMustBeFitForDockedFigure
-perm = randperm(numel(labelsTrain), 25);
-subset = imgDataTrain(:, :, 1, perm);
-montage(subset);
-truesize([150 150]);
-end
